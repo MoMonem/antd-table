31ae5cd93dceb66e23fef0a49061f53658830481
MoMonem <Mahmoudmonem0@gmail.com>
2023-01-13 23:41:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,432 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  Modal,
+  Form,
+  Input,
+  Select,
+  DatePicker,
+  Space,
+  Table,
+  Popconfirm,
+  message,
+} from "antd";
+import { useEffect, useState } from "react";
+
+message.config({
+  duration: 1,
+  maxCount: 3,
+});
 
 function App() {
+  const [form] = Form.useForm(),
+    [open, setOpen] = useState(false),
+    { RangePicker } = DatePicker,
+    { TextArea } = Input,
+    { Option } = Select;
+
+  const [title, setTitle] = useState(""),
+    [type, setType] = useState(""),
+    [startDate, setStartDate] = useState(""),
+    [endDate, setEndDate] = useState(""),
+    [description, setDescription] = useState(""),
+    [data, setData] = useState([]),
+    [editingKey, setEditingKey] = useState("");
+
+  const columns = [
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+      render: (text, record) => {
+        if (record.id === editingKey) {
+          return (
+            <Input
+              style={{
+                width: "100%",
+              }}
+              value={title}
+              placeholder={text}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          );
+        } else {
+          return <p>{text}</p>;
+        }
+      },
+    },
+    {
+      title: "Type",
+      dataIndex: "type",
+      key: "type",
+      render: (text, record) => {
+        if (record.id === editingKey) {
+          return (
+            <Select
+              style={{
+                width: "100%",
+              }}
+              value={type}
+              placeholder={text}
+              onChange={(e) => setType(e)}
+              options={[
+                {
+                  value: "generic",
+                  label: "Generic",
+                },
+                {
+                  value: "holiday",
+                  label: "Holiday",
+                },
+                {
+                  value: "jack",
+                  label: "Jack",
+                },
+                {
+                  value: "lucy",
+                  label: "Lucy",
+                },
+                {
+                  value: "Yiminghe",
+                  label: "yiminghe",
+                },
+              ]}
+            />
+          );
+        } else {
+          return <p>{text}</p>;
+        }
+      },
+    },
+    {
+      title: "Start Date",
+      dataIndex: "startDate",
+      defaultSortOrder: "ascend",
+      sorter: (a, b) => Date.parse(a.startDate) - Date.parse(b.startDate),
+      key: "startDate",
+      render: (text, record) => {
+        if (record.id === editingKey) {
+          return (
+            <DatePicker
+              style={{
+                width: "100%",
+              }}
+              placeholder={text}
+              onChange={(e) => setStartDate(e.format("YYYY-MM-DD"))}
+            />
+          );
+        } else {
+          return <p>{text}</p>;
+        }
+      },
+    },
+    {
+      title: "End Date",
+      dataIndex: "endDate",
+      defaultSortOrder: "ascend",
+      sorter: (a, b) => Date.parse(a.endDate) - Date.parse(b.endDate),
+      key: "endDate",
+      render: (text, record) => {
+        if (record.id === editingKey) {
+          return (
+            <DatePicker
+              style={{
+                width: "100%",
+              }}
+              placeholder={text}
+              onChange={(e) => setEndDate(e.format("YYYY-MM-DD"))}
+            />
+          );
+        } else {
+          return <p>{text}</p>;
+        }
+      },
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+      render: (text, record) => {
+        if (record.id === editingKey) {
+          return (
+            <TextArea
+              style={{
+                width: "100%",
+              }}
+              rows={1}
+              value={description}
+              placeholder={text}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          );
+        } else {
+          return <p>{text}</p>;
+        }
+      },
+    },
+    {
+      title: "Action",
+      key: "id",
+      render: (record) => {
+        if (record.id === editingKey) {
+          return (
+            <Space
+              style={{
+                cursor: "pointer",
+                color: "blue",
+              }}
+              size="middle"
+            >
+              <p onClick={() => setEditingKey("")}>Cancel</p>
+              <Popconfirm
+                title="Are you Sure?"
+                okText="Yes"
+                cancelText="No"
+                onConfirm={handleUpdate}
+              >
+                <p>Update</p>
+              </Popconfirm>
+            </Space>
+          );
+        } else {
+          return (
+            <Space
+              style={{
+                cursor: "pointer",
+                color: "blue",
+              }}
+              size="middle"
+            >
+              <p onClick={() => setEditingKey(record.id)}>Edit</p>
+              <Popconfirm
+                title="Are you Sure?"
+                okText="Yes"
+                cancelText="No"
+                onConfirm={() => handleDelete(record.id)}
+              >
+                <p>Delete</p>
+              </Popconfirm>
+            </Space>
+          );
+        }
+      },
+    },
+  ];
+
+  // fetch data on reload
+  useEffect(() => {
+    fetch("http://localhost:3000/events")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => message.error(err));
+  }, []);
+
+  // create event
+  const handleCreate = (values) => {
+    fetch("http://localhost:3000/events", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: values.title,
+        type: values.type,
+        startDate: values.date[0].format("YYYY-MM-DD"),
+        endDate: values.date[1].format("YYYY-MM-DD"),
+        description: values.description,
+      }),
+    })
+      .then((res) => {
+        if (res.status === 201) {
+          setOpen(false);
+          form.resetFields();
+          setTimeout(() => {
+            message.success("Event Added Successfully");
+            setTimeout(() => {
+              window.location.reload();
+            }, 800);
+          }, 500);
+        }
+      })
+      .catch((err) => message.error(err));
+  };
+
+  // delete an event
+  const handleDelete = (id) => {
+    fetch(`http://localhost:3000/events/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(() => {
+        message.success("Event Deleted Successfully");
+        setTimeout(() => {
+          window.location.reload();
+        }, 500);
+      })
+      .catch((err) => message.error(err));
+  };
+
+  const handleUpdate = () => {
+    let rawinEditMode = {};
+    if (title !== "") rawinEditMode.title = title;
+    if (type !== "") rawinEditMode.type = type;
+    if (startDate !== "") rawinEditMode.startDate = startDate;
+    if (endDate !== "") rawinEditMode.endDate = endDate;
+    if (description !== "") rawinEditMode.description = description;
+
+    fetch(`http://localhost:3000/events/${editingKey}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...rawinEditMode }),
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          setEditingKey("");
+          setTimeout(() => {
+            message.success("Event Updated Successfully");
+            setTimeout(() => {
+              window.location.reload();
+            }, 800);
+          }, 500);
+        }
+      })
+      .catch((err) => message.error(err));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Space direction="vertical" style={{ width: "100%" }}>
+      <Space
+        direction="horizontal"
+        style={{
+          padding: "20px",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Input
+          placeholder="Search for an event title or description"
+          onChange={(e) => {
+            fetch("http://localhost:3000/events")
+              .then((res) => res.json())
+              .then((data) => {
+                const finalData = data.filter((item) => {
+                  if (
+                    item.title
+                      .toLowerCase()
+                      .includes(e.target.value.toLowerCase()) ||
+                    item.description
+                      .toLowerCase()
+                      .includes(e.target.value.toLowerCase())
+                  )
+                    return item;
+                });
+                setData(finalData);
+              })
+              .catch((err) => message.error(err));
+          }}
+          style={{
+            width: "350px",
+          }}
+        />
+        <Button type="primary" onClick={() => setOpen(true)}>
+          Create Event
+        </Button>
+      </Space>
+      <Modal
+        data-testid="modal"
+        open={open}
+        title="Create Event"
+        okText="Create"
+        onCancel={() => {
+          setOpen(false);
+        }}
+        onOk={() => {
+          form
+            .validateFields()
+            .then((values) => {
+              form.resetFields();
+              handleCreate(values);
+            })
+            .catch((info) => {
+              console.log("Validate Failed", info);
+            });
+        }}
+      >
+        <Form
+          form={form}
+          layout="vertical"
+          name="form_in_modal"
+          data-testid="create-form"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Form.Item
+            name="title"
+            label="Title"
+            rules={[
+              {
+                required: true,
+                message: "Required field",
+              },
+            ]}
+          >
+            <Input placeholder="Add a title here" data-testid="title" />
+          </Form.Item>
+          <Form.Item
+            label="Type"
+            name="type"
+            rules={[
+              {
+                required: true,
+                message: "Required field",
+              },
+            ]}
+          >
+            <Select
+              data-testid="type"
+              showSearch={true}
+              placeholder="Select a type"
+            >
+              <Option value="generic">Generic</Option>
+              <Option value="holiday">Holiday</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="Date"
+            name="date"
+            rules={[
+              {
+                required: true,
+                message: "Required field",
+              },
+            ]}
+          >
+            <RangePicker
+              data-testid="date"
+              style={{
+                width: "100%",
+              }}
+              placeholder={["Start date", "End date"]}
+            />
+          </Form.Item>
+          <Form.Item label="Description" name="description">
+            <TextArea
+              rows={4}
+              placeholder="Add description here"
+              data-testid="description"
+            />
+          </Form.Item>
+        </Form>
+      </Modal>
+      <Table
+        data-testid="ant-table"
+        columns={columns}
+        dataSource={data}
+        rowKey="id"
+        pagination={{ pageSize: 10 }}
+      />
+    </Space>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,95 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(cleanup);
+
+describe("app", () => {
+  window.matchMedia =
+    window.matchMedia ||
+    function () {
+      return {
+        matches: false,
+        addListener: function () {},
+        removeListener: function () {},
+      };
+    };
+
+  it("Table gets filled with the correct data", () => {
+    render(<App />);
+
+    const antTable = screen.getByTestId("ant-table");
+    const allRows = antTable.querySelectorAll("tr");
+
+    expect(antTable).toBeInTheDocument();
+    expect(allRows.length).toBe(2);
+  });
+
+  it("Modal and form show on button click", async () => {
+    render(<App />);
+
+    fireEvent.click(screen.getByText("Create Event"));
+
+    // Modal and table visability
+    expect(screen.getByTestId("modal")).toBeInTheDocument();
+    expect(screen.getByTestId("create-form")).toBeInTheDocument();
+    expect(screen.getByTestId("title")).toBeInTheDocument();
+    expect(screen.getByTestId("type")).toBeInTheDocument();
+    expect(screen.getByTestId("date")).toBeInTheDocument();
+    expect(screen.getByTestId("description")).toBeInTheDocument();
+  });
+
+  it("form validation appear if failed", async () => {
+    render(<App />);
+
+    fireEvent.click(screen.getByText("Create Event"));
+    fireEvent.click(screen.getByText("Create"));
+    await new Promise((r) => setTimeout(r, 1000));
+    expect(screen.getAllByRole("alert").length).toBeGreaterThan(0);
+  });
+
+  it("Form submits the data correctly and data displays in table", async () => {
+    render(<App />);
+
+    fireEvent.click(screen.getByText("Create Event"));
+
+    // Fill in form
+    fireEvent.change(screen.getByPlaceholderText("Add a title here"), {
+      target: { value: "Jest test Event Title" },
+    });
+    expect(screen.getByPlaceholderText("Add a title here").value).toBe(
+      "Jest test Event Title"
+    );
+
+    fireEvent.click(screen.getByRole("combobox"));
+    fireEvent.change(screen.getByRole("combobox"), {
+      target: { value: "Holiday" },
+    });
+    fireEvent.keyDown(screen.getByRole("combobox"), {
+      key: "Enter",
+      code: "Enter",
+      charCode: 13,
+    });
+
+    fireEvent.change(screen.getByPlaceholderText("Start date"), {
+      target: { value: "2023-02-01 - 2023-02-01" },
+    });
+    expect(screen.getByPlaceholderText("Start date").value).toBe(
+      "2023-02-01 - 2023-02-01"
+    );
+
+    fireEvent.change(screen.getByPlaceholderText("Add description here"), {
+      target: { value: "Jest test Event Description" },
+    });
+    expect(screen.getByPlaceholderText("Add description here").value).toBe(
+      "Jest test Event Description"
+    );
+
+    // Submit form
+    fireEvent.submit(screen.getByText("Create"));
+    await new Promise((r) => setTimeout(r, 500));
+
+    expect(
+      screen.getByTestId("ant-table").querySelectorAll("tr").length
+    ).toBeGreaterThan(2);
+  });
 });
